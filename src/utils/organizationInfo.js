9fc92df9b0c36a02071ae3eef50a9722f635ca71
Nikola Crhák <nikola.crhak@gmail.com>
2026-07-22 10:33:14 +0200

    Povinné údaje webu

---
+++ src/utils/organizationInfo.js
@@ -0,0 +1,9 @@
+export const ORGANIZATION = {
+  name: 'CTRL Europe, z. s.',
+  address: 'Nové sady 988/2, Staré Brno, 602 00 Brno',
+  ico: '29750466',
+  registrationCs:
+    'Spolek je zapsán ve spolkovém rejstříku vedeném u Krajského soudu v Brně, oddíl L, vložka 32208.',
+  registrationEn:
+    'Registered in the Register of Associations maintained by the Regional Court in Brno, Section L, Insert 32208.',
+};
